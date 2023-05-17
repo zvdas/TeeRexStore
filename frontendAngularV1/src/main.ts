126e4eeb7eac5969111c6e4cd5f0baa07bc1e4d8
zvdas <rodriguezzude@gmail.com>
2023-05-17 11:03:34 +0530

    bootstrap imported into main.ts & styles.css files

--- frontendAngularV1/src/main.ts
+++ frontendAngularV1/src/main.ts
@@ -1,6 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
